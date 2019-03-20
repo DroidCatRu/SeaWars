ca6f56b484e32ffa6c2d18f3c50028563c52092e
Артем Кошкин <artko2050@gmail.com>
2019-03-20 21:48:40 +0300

    Code refactoring

--- SeaWars-test1/SeaWars-test1.cpp
+++ SeaWars-test1/SeaWars-test1.cpp
@@ -6,54 +6,7 @@
 
 using namespace std;
 
-/*void show_board(cell board[10][10]) {
-	system("CLS");
-	cout << "It's your board and board of your enemy\n";
-	cout << "   ";
-	for (char i = 'A'; i <= 'A' + 9; i++) {
-		cout << setw(3) << i;
-	}
-	cout << "     ";
-	for (char i = 'A'; i <= 'A' + 9; i++) {
-		cout << setw(3) << i;
-	}
-	cout << endl;
-
-	for (int i = 0; i < 10; i++) {
-		cout << setw(3) << i + 1;
-		for (int g = 0; g < 10; g++) {
-			cout << setw(3) << board[g][i].getStatus();
-		}
-
-		cout << setw(5) << i + 1;
-		for (int g = 0; g < 10; g++) {
-			cout << setw(3) << board[g][i].getStatus();
-		}
-
-		cout << endl;
-	}
-}*/
-
 int main() {
-	/*cell board[10][10];
-	show_board(board);
-	int x, y, len;
-	char dir;
-	cout << "Enter x, y, length and direction(h or v)" << endl;
-	cin >> x >> y >> len >> dir;
-	ship ship1(x, y, len, dir);
-	for (;;) {
-		int x, y;
-		cin >> x >> y;
-		int state = rand() % 2 + 1;
-		if (state == 1) {
-			board[x - 1][y - 1].setShooted();
-		}
-		else {
-			board[x - 1][y - 1].setEmpty();
-		}
-		show_board(board);
-	}*/
 
 	Game game;
 
